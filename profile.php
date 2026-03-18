336f53a573330c448016d0c11db395901569c293
Valerchik <abkadyrov.ilnar@mail.ru>
2026-03-18 12:14:09 +0500

    add modal window

--- profile.php
+++ profile.php
@@ -22,49 +22,57 @@ if (!isset($_SESSION['email'])){
         <p><? echo $_SESSION["id"] ?></p>
 
         <button onclick="form_vision()">Изменить данные</button>
-
-            <section id="form_section">
-                <form id='form' action="/update" method="post">
-                <div class="form-group">
-                <label for="name">Имя:</label>
-                <input type="text" name="name" placeholder="Иванов Иван Иванович" required>
-                </div>
-
-                <div class="form-group">
-                <label for="password">Пароль:</label>
-                <input id='password1' type="password" name="password1" placeholder="Пароль" required>
-                </div>
-
-                <div class="form-group">
-                <label for="password">Подтвердите пароль:</label>
-                <input id='password2' type="password" name="password2" placeholder="Пароль" required>
-                </div>
-
-                <div class="form-group">
-                <label for="phone">Телефон:</label>
-                <input type="text" name="phone" placeholder="Иванов Иван Иванович" required>
-                </div>
-
-                <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" name="email" placeholder="mail@mail.ru" required>
-                </div>
-
-                <div class="form-group">
-                <input id='button' type="submit" value="Обновить">
-                </div>
-                </form>
-            </section>
     </main>
+
+            <modal id="form_section">
+                <div class="modal-content">
+                    <button onclick="delete_section()" style="float: right;">
+                        X
+                    </button>
+                    <form id='form' action="/update" method="post">
+                    <div class="form-group">
+                    <label for="name">Имя:</label>
+                    <input type="text" name="name" placeholder="Иванов Иван Иванович" required>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="password">Пароль:</label>
+                    <input id='password1' type="password" name="password1" placeholder="Пароль" required>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="password">Подтвердите пароль:</label>
+                    <input id='password2' type="password" name="password2" placeholder="Пароль" required>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="phone">Телефон:</label>
+                    <input type="text" name="phone" placeholder="Иванов Иван Иванович" required>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" placeholder="mail@mail.ru" required>
+                    </div>
+
+                    <div class="form-group">
+                    <input id='button' type="submit" value="Обновить">
+                    </div>
+                    </form>
+                </div>
+            </modal>
 
     <? require_once 'patterns/footer.php'; ?>
 
     <script>
         const form = document.querySelector("#form_section");
-        form.style.visibility = "hidden";
 
         function form_vision(){
-            form.style.visibility = "visible";
+            form.style.display = "block";
+        }
+
+        function delete_section(){
+            form.style.display = "none";
         }
     </script>
 </body>
